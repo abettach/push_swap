c529148a7114895910f1345a6dbeab0a1405ecb0
abettach <bettachiachraf@gmail.com>
2021-07-05 12:43:40 +0100

    push_swap

--- start.sh
+++
@@ -1,5 +0,0 @@
-gcc -c *.c push_swap_programme/*.c && gcc *.o *.h -o push_swap && rm *.o
-gcc -c *.c checker_programme/*.c && gcc *.o *.h -o checker && rm *.o
-export ARG="5 4 1 100 200"
-./push_swap $ARG
-./push_swap $ARG | ./checker $ARG
